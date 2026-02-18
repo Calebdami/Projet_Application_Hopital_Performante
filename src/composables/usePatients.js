29b96b951118508f6f7bd68090b3ba1fb64b8409
Caleb KPANOU <caleb.kpanou@chem.loc>
2026-02-18 16:38:43 +0100

    67%

--- src/composables/usePatients.js
+++ src/composables/usePatients.js
@@ -1,32 +1,7 @@
-import { useLocalStorage } from './useLocalStorage'
+import { ref, watch } from 'vue'
 
-export function usePatients() {
-    const patients = useLocalStorage('patients', [])
-
-    function addPatient(patient) {
-        patients.value.push({
-            id: Date.now(),
-            createdAt: new Date().toISOString(),
-            ...patient
-        })
-    }
-
-    function updatePatient(id, updatedPatient) {
-        const index = patients.value.findIndex(p => p.id === id)
-        if (index !== -1) {
-            patients.value[index] = { ...patients.value[index], ...updatedPatient }
-        }
-    }
-
-    function deletePatient(id) { patients.value = patients.value.filter(p => p.id !== id) }
-
-    function findPatient(id) { return patients.value.find(p => p.id === id) }
-
-    return {
-        patients,
-        addPatient,
-        updatePatient,
-        deletePatient,
-        findPatient
-    }
+export function useLocalStorage(key, defaultValue = []) {
+    const data = ref(JSON.parse(localStorage.getItem(key)) || defaultValue)
+    watch(data, (newVal) => { localStorage.setItem(key, JSON.stringify(newVal)) }, { deep: true })
+    return data
 }
